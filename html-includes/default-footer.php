6efe3b60402a39f808c583b26ca1ad98bd5f7896
spelgubbe <jacce96@gmail.com>
2016-06-14 17:20:50 +0200

    update

--- html-includes/default-footer.php
+++ html-includes/default-footer.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<div class="content has-text-centered">
 		<p>
-			<strong>Made</strong> by <a href="http://jkr.rocks">Spelgubbe</a>
+			<strong>Made</strong> by <a href="http://jakobkratz.com">spelgubbe</a> &amp; <a href="http://johannesekman.com">mby</a>
 		</p>
 		<p>
 			<a class="icon" href="https://github.com/spelgubbe">
--- html-includes/default-stylesheets.php
+++ html-includes/default-stylesheets.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" type="text/css" href="style.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">-->
